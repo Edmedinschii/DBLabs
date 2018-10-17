0cfb1218fb7df03de5ef5bb488c6272eb777f4ab
Edmedinschii <34598401+Edmedinschii@users.noreply.github.com>
2018-10-17 10:22:05 +0300

    Update ex6.sql

--- lab4/ex6.sql
+++ lab4/ex6.sql
@@ -1,4 +1,4 @@
-select TOP 5 studenti.Nume_Student , studenti.Prenume_Student , Nota
+select  TOP(5) WITH TIES studenti.Nume_Student , studenti.Prenume_Student , Nota
 from studenti
 join studenti_reusita on studenti.Id_Student = studenti_reusita.Id_Student
 join discipline on discipline.Id_Disciplina = studenti_reusita.Id_Disciplina
