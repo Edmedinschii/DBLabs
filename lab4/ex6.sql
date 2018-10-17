7f19f20416ad27e61ad45a8189e5f6c7cc965bcf
Edmedinschii <34598401+Edmedinschii@users.noreply.github.com>
2018-10-17 12:03:38 +0300

    Update ex6.sql

--- lab4/ex6.sql
+++ lab4/ex6.sql
@@ -1,3 +1,9 @@
+/*
+	Afisati numele si prenumile primilor 5 studentii care au obtinut note in ordine descrescatoare 
+	la al doilea test de la disciplina Baze de date. Sa se foloseasc optiunea Top...with ties.
+*/
+
+
 select  TOP(5) WITH TIES studenti.Nume_Student , studenti.Prenume_Student , Nota
 from studenti
 join studenti_reusita on studenti.Id_Student = studenti_reusita.Id_Student
