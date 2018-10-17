744abd2fb2b77847b88abe5457962642d84a1fa3
Edmedinschii <34598401+Edmedinschii@users.noreply.github.com>
2018-10-17 12:03:14 +0300

    Update ex35.sql

--- lab4/ex35.sql
+++ lab4/ex35.sql
@@ -1,3 +1,8 @@
+/*
+    Gasiti denumirele disciplinelor si media notelor pe disciplina. Afisati numai disciplinele cu medii mai mare de 7.0
+*/
+
+
 select Disciplina,studenti_reusita.Id_Disciplina, avg(cast(studenti_reusita.Nota as float)) as Average 
 from studenti_reusita 
 join discipline on studenti_reusita.Id_Disciplina = discipline.Id_Disciplina 
