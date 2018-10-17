caadc29f78f6460f53a9a9658a067cb5f2f334f8
Edmedinschii <34598401+Edmedinschii@users.noreply.github.com>
2018-10-17 12:02:16 +0300

    Update 26.sql

--- lab4/26.sql
+++ lab4/26.sql
@@ -1,3 +1,7 @@
+/*
+	Gasiti numele, prenumele, adresa studentilor si ale profesori care locuiesc pe strada 31 August.
+*/
+
 select distinct studenti.Nume_Student , studenti.Prenume_Student , profesori.Nume_Profesor,profesori.Prenume_Profesor 
 from studenti
 join studenti_reusita on studenti_reusita.Id_Student = studenti.Id_Student
